bb5986e38c026411c379f18f89e2f41179fd9bc5
Brooke Perry and Daniel Bennett <daniel@>
2019-03-04 14:10:57 -0800

    refactor forms

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import * as types from './../constants/ActionTypes';
+import v4 from 'uuid/v4';
+import API_KEY from './../../env'
 
 export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
@@ -14,3 +16,22 @@ export const changeSong = (newSelectedSongId) => ({
   type: types.CHANGE_SONG,
   newSelectedSongId
 });
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=' + API_KEY).then(
+      response => response.json('An error occured.', error)
+    ).then(function(json) {
+      console.log('Check it out', json)
+    });
+  };
+}
+
+  export const requestSong = (title, localSongId) => ({
+    type: types.REQUEST_SONG,
+    title,
+    songId: localSongId
+  });
